f1aa495ba56ded839c622de94f68ba2d23a72d33
Lemyr27 <sander.96@bk.ru>
2024-03-23 00:16:23 +0500

    Added and configured navigation

--- app/src/main/java/ru/vladburchinskiy/lab5/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/ru/vladburchinskiy/lab5/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Здесь будет викторина"
     }
     val text: LiveData<String> = _text
 }
